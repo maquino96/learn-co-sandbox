9dc25ae2a000769fc56bd719bcaf2eb536c8e019
Matthew Aquino <matthewaquino96@gmail.com>
2021-02-08 02:43:02 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,5 @@
+meals = {:breakfast => "cereal", :lunch => "peanut butter and jelly sandwich", :dinner => "mushroom risotto"}
+#=> {:breakfast=>"cereal", :lunch=>"peanut butter and jelly sandwich", :dinner=>"mushroom risotto"}
+ 
+puts meals[:breakfast]
+#=> "cereal"
